1434b80afebffa2520b13c6f5848870fdc7bde99
Lemeri02 <9588504@gmail.com>
2020-05-22 18:27:43 +0500

    add bot

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -6,12 +6,18 @@ TOKEN = ENV['TOKEN'] || Rails.application.credentials.bot[:token]
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
 
+    kb = [
+      Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
+      Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Touch me', callback_data: 'touch'),
+      Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Switch to inline', switch_inline_query: 'some text')
+    ]
+    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
     case message.text
     when '/add'
       current_user = User.where(telegram_id: message.from.id).first_or_create do |user|
         user.username = message.from.username
       end
-      bot.api.send_message(chat_id: message.chat.id, text: "Hi, I add User #{current_user.username}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hi, I add User #{current_user.username}", reply_markup: markup )
     when '/delete'
       bot.api.send_message(chat_id: message.chat.id, text: '/delete')
     end
